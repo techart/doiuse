f239085addd00873678b9c918fbd3e5b9a7d27f5
Anton Shvydkoy <shvydkoy@techart.ru>
2016-10-25 09:39:15 +0300

    hack

--- src/doiuse.js
+++ src/doiuse.js
@@ -3,82 +3,92 @@ let missingSupport = require('./missing-support');
 let Detector = require('./detect-feature-use');
 let Multimatch = require('multimatch');
 
-function doiuse (options) {
-  let {browsers: browserQuery, onFeatureUsage, ignore: ignoreOptions, ignoreFiles} = options;
+function doiuse(options) {
+	let {browsers: browserQuery, onFeatureUsage, ignore: ignoreOptions, ignoreFiles} = options;
 
-  if (!browserQuery) {
-    browserQuery = doiuse['default'].slice()
-  }
-  let {browsers, features} = missingSupport(browserQuery);
-  let detector = new Detector(_.keys(features));
+	if (!browserQuery) {
+		browserQuery = doiuse['default'].slice()
+	}
 
-  return {
-    info () {
-      return {
-        browsers: browsers,
-        features: features
-      }
-    },
+	if (!onFeatureUsage) {
+		onFeatureUsage = function onlyMissingSupport(usageInfo) {
+			return usageInfo.featureData.missing;
+		};
+	}
 
-    postcss (css, result) {
-      return detector.process(css, function ({feature, usage, ignore}) {
-        if (ignore && ignore.indexOf(feature) !== -1) {
-          return
-        }
-        if (ignoreOptions && ignoreOptions.indexOf(feature) !== -1) {
-          return
-        }
+	let {browsers, features} = missingSupport(browserQuery);
+	let detector = new Detector(_.keys(features));
 
-        if (ignoreFiles && Multimatch(usage.source.input.from, ignoreFiles).length > 0) {
-          return
-        }
+	return {
+		info () {
+			return {
+				browsers: browsers,
+				features: features
+			}
+		},
 
-        let messages = [];
-        if (features[feature].missing) {
-          messages.push('not supported by: ' + features[feature].missing)
-        }
-        if (features[feature].partial) {
-          messages.push('only partially supported by: ' + features[feature].partial)
-        }
+		postcss (css, result) {
+			return detector.process(css, function ({feature, usage, ignore}) {
+				if (ignore && ignore.indexOf(feature) !== -1) {
+					return;
+				}
 
-        let message = features[feature].title + ' ' + messages.join(' and ') + ' (' + feature + ')';
+				if (ignoreOptions && ignoreOptions.indexOf(feature) !== -1) {
+					return;
+				}
 
-        let warn = function resultWarn() {
-          result.warn(message, { node: usage, plugin: 'doiuse' });
-        };
+				let loc = usage.source;
+				loc.original = css.source.input.map ? {
+					start: css.source.input.map.consumer().originalPositionFor(loc.start),
+					end: css.source.input.map.consumer().originalPositionFor(loc.end)
+				} : {
+					start: loc.start,
+					end: loc.end
+				};
 
-        if (onFeatureUsage) {
-          let loc = usage.source;
-          loc.original = css.source.input.map ? {
-            start: css.source.input.map.consumer().originalPositionFor(loc.start),
-            end: css.source.input.map.consumer().originalPositionFor(loc.end)
-          } : {
-            start: loc.start,
-            end: loc.end
-          };
+				let filepath = (loc.original.start.source || loc.input.file || loc.input.from);
 
-          message = (loc.original.start.source || loc.input.file || loc.input.from) + ':' +
-            loc.original.start.line + ':' + loc.original.start.column + ': ' + message;
+				if (ignoreFiles && Multimatch(filepath, ignoreFiles).length > 0) {
+					return;
+				}
 
-          if (onFeatureUsage({
-                feature: feature,
-                featureData: features[feature],
-                usage: usage,
-                message: message
-            })) {
-            warn();
-          }
-        } else {
-          warn();
-        }
-      })
-    }
-  }
+				let messages = [];
+				if (features[feature].missing) {
+					messages.push('not supported by: ' + features[feature].missing)
+				}
+				if (features[feature].partial) {
+					messages.push('only partially supported by: ' + features[feature].partial)
+				}
+
+				let message = features[feature].title + ' ' + messages.join(' and ') + ' (' + feature + ')';
+
+				let warn = function resultWarn() {
+					result.warn(message, {node: usage, plugin: 'doiuse'});
+				};
+
+				if (onFeatureUsage) {
+
+					message = filepath + ':' + loc.original.start.line + ':' + loc.original.start.column + ': ' + message;
+
+					if (onFeatureUsage({
+							feature: feature,
+							featureData: features[feature],
+							usage: usage,
+							message: message
+						})) {
+						warn();
+					}
+				} else {
+					warn();
+				}
+			})
+		}
+	}
 }
 doiuse['default'] = [
-  '> 1%',
-  'last 2 versions',
-  'Firefox ESR',
-  'Opera 12.1'
+	'> 1%',
+	'last 2 versions',
+	'Firefox ESR',
+	'Opera 12.1'
 ]
 module.exports = doiuse
